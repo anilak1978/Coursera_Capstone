35cd6f1ab33102484180cf385858aca65ad06821
Anil Akyildirim <anilak1978@gmail.com>
2018-12-13 05:17:27 -0500

    Coursera_Capstone_Project.py

---
+++ Coursera_Capstone_Project.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # Coursera Capstone Project
+
+# ### This notebook will be mainly used for Capstone Project. We will be usinf Foursquare as the location data provider as it is the most comprehensive one. Foursquare dev account is also easy to create.
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
